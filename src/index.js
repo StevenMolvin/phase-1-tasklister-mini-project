27557c9fcbce826bf8aa9b94477be9e46c98fb7b
StevenMo <stevenmolvin@gmail.com>
2023-03-23 01:50:00 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+const taskList = document.getElementById('tasks');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const taskDescription = document.getElementById('new-task-description').value;
+  const taskElement = document.createElement('li');
+  taskElement.textContent = taskDescription;
+  taskList.appendChild(taskElement);
+  document.getElementById('new-task-description').value = '';
+});
 });
